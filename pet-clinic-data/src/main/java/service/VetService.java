075886c3a1da8ee3823d59a5f1d5bfec0f37e7e5
wlwlsus <159632_@naver.com>
2022-02-24 14:10:00 +0900

    [refactor]common crud interface. closes #11

--- pet-clinic-data/src/main/java/service/VetService.java
+++ pet-clinic-data/src/main/java/service/VetService.java
@@ -4,11 +4,5 @@ import model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-
-  Vet findById(Long id);
-
-  Vet save(Vet vet);
-
-  Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
