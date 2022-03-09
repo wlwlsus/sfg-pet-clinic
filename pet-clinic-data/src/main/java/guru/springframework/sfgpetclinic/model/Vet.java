feb981fa460c96a00b96f4d00ff460ec2e7dbe15
wlwlsus <159632_@naver.com>
2022-03-09 22:36:43 +0900

    [update] Vet to use JPA

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,18 +1,25 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Vet extends Person{
+@Entity
+@Table(name = "vets")
+public class Vet extends Person {
 
-  private Set<Speciality> specialities = new HashSet<>();
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "vet_specialties",
+					joinColumns = @JoinColumn(name = "vet_id"),
+					inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+	private Set<Speciality> specialities = new HashSet<>();
 
-  public Set<Speciality> getSpecialities() {
-    return specialities;
-  }
+	public Set<Speciality> getSpecialities() {
+		return specialities;
+	}
 
-  public void setSpecialities(Set<Speciality> specialities) {
-    this.specialities = specialities;
-  }
+	public void setSpecialities(Set<Speciality> specialities) {
+		this.specialities = specialities;
+	}
 }
 
