c40efb29e428b95b609b122b155592b2b502f672
wlwlsus <159632_@naver.com>
2022-03-09 22:23:32 +0900

    [update] Owner with JPA Entity Mappings. closes #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,13 +1,24 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
 
+  @Column(name = "address")
   private String address;
+
+  @Column(name = "city")
   private String city;
+
+  @Column(name = "telephone")
   private String telephone;
+
+  // mappedBy로 pets 와 owner 가 연관되어 있다는 것을 알려준다.
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
   private Set<Pet> pets = new HashSet<>();
 
   public String getAddress() {
