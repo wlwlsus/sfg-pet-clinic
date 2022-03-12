f8dcf537aa02bea5e9d077e9e614951ad2521c23
wlwlsus <159632_@naver.com>
2022-03-13 01:22:17 +0900

    [add] CRUD Test for Owner Map Service. closes #50

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/OwnerMapService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-@Profile({"default","map"})
+@Profile({"default", "map"})
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
 
 	private final PetTypeService petTypeService;
@@ -35,18 +35,18 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 	@Override
 	public Owner save(Owner object) {
 
-		if(object != null){
+		if (object != null) {
 			if (object.getPets() != null) {
 				object.getPets().forEach(pet -> {
-					if (pet.getPetType() != null){
-						if(pet.getPetType().getId() == null){
+					if (pet.getPetType() != null) {
+						if (pet.getPetType().getId() == null) {
 							pet.setPetType(petTypeService.save(pet.getPetType()));
 						}
 					} else {
 						throw new RuntimeException("Pet Type is required");
 					}
 
-					if(pet.getId() == null){
+					if (pet.getId() == null) {
 						Pet savedPet = petService.save(pet);
 						pet.setId(savedPet.getId());
 					}
@@ -72,6 +72,10 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 
 	@Override
 	public Owner findByLastName(String lastName) {
-		return null;
+		return this.findAll()
+						.stream()
+						.filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
+						.findFirst()
+						.orElse(null);
 	}
 }
