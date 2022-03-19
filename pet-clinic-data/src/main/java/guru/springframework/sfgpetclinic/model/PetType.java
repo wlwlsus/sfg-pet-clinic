887bc15e237647f896329e4b9ecb2ea985b4119e
swj <159632_@naver.com>
2022-03-19 12:10:46 +0900

    Add : display owner. closes #54

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -16,6 +16,11 @@ import javax.persistence.Table;
 @Table(name = "types")
 public class PetType extends BaseEntity {
 
-  @Column(name = "name")
-  private String name;
+	@Column(name = "name")
+	private String name;
+
+	@Override
+	public String toString() {
+		return name;
+	}
 }
