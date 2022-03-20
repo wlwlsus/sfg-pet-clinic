ddd713c7ccfc62e9e647be184fc1faedf15793da
wlwlsus <159632_@naver.com>
2022-03-20 15:43:28 +0900

    Feat : Create, Update Owner. closes #56

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
+
 @Setter
 @Getter
 @NoArgsConstructor
@@ -14,7 +15,12 @@ import java.io.Serializable;
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	public boolean isNew() {
+		return this.id == null;
+	}
+
 }
