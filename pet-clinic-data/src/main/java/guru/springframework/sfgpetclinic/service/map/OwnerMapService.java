4109caa73c7ef31c8af5e578edc3ace68b9be61e
wlwlsus <159632_@naver.com>
2022-03-20 13:36:06 +0900

    Feat : Impl find owner. close #55

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/OwnerMapService.java
@@ -8,6 +8,7 @@ import guru.springframework.sfgpetclinic.service.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -77,5 +78,10 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 						.filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
 						.findFirst()
 						.orElse(null);
+	}
+
+	@Override
+	public List<Owner> findAllByLastNameLike(String lastName) {
+		return null;
 	}
 }
